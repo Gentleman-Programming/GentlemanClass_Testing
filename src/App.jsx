8c1701691a49fe62d7a8fcf676cb5ed7964e2320
Migue Martelo <miguemartelo1031@gmail.com>
2022-08-01 22:07:17 -0500

    feat: add login form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <Box className='App'>
-      <Typography variant='h2'>GentlemanClass - Testing</Typography>
+    <Box
+      className='App'
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      flexDirection='column'
+    >
+      <Typography variant='h2' mb='20px' textAlign='center'>
+        GentlemanClass - Testing
+      </Typography>
+      <Box
+        sx={{
+          bgcolor: 'grey.300',
+          borderRadius: '30px',
+          height: '250px',
+          p: '50px',
+          width: '50%',
+        }}
+      >
+        <form>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
+            <TextField variant='outlined' label='Nombre de usuario' fullWidth />
+            <TextField
+              variant='outlined'
+              label='Contraseña'
+              type='password'
+              fullWidth
+            />
+            <Button fullWidth variant='contained'>
+              Login
+            </Button>
+          </Box>
+        </form>
+      </Box>
     </Box>
   );
 }
