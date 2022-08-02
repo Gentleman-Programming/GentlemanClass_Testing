4aeb9def330b20012d8e410a219f3258595344f5
Migue Martelo <miguemartelo1031@gmail.com>
2022-08-01 21:33:48 -0500

    chore: create the initial config with vite and MUI

---
+++ README.md
@@ -0,0 +1,1 @@
+# GentlemanClass - Testing
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <Box className='App'>
+      <Typography variant='h2'>GentlemanClass - Testing</Typography>
+    </Box>
+  );
+}
+
+export default App;
